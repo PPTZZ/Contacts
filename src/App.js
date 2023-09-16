ebb4fa3b65085584c7f04ccf7b90d551f307d892
PPTZ <poputa.alex@gmail.com>
2023-09-16 17:38:15 +0300

    added full list of robots

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import CardList from "./CardList"
+import { robots } from "./robots"
+import SearchBox from "./SearchBox.js"
+
+
+const App = () =>{
+    
+    return (
+        <div className="tc">
+        <h1>Robo Friends</h1>
+        <SearchBox />
+        <CardList robots={ robots } />
+        </div>
+    );
+}
+
+export default App;
