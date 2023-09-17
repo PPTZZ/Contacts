4c1823a2a61d4aeaa03c0df656edb232aec9ca4e
PPTZ <poputa.alex@gmail.com>
2023-09-17 14:55:05 +0300

    legat la fake server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react"
 import CardList from "./CardList"
-import { robots } from "./robots"
 import SearchBox from "./SearchBox.js"
 import "./title.css"
 
@@ -13,6 +12,15 @@ class App extends React.Component{
             robots: [],
             searchfield : '',
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            return response.json();
+        }).then(users =>{
+            this.setState({robots:users})
+        })
+    
     }
 
     onSearchChange = (event) =>{
